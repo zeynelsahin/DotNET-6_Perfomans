f70426319e4673e64bf8449199f3ed7e9c71f2a1
zeynelsahin <zynl.shn@hotmail.com>
2023-09-11 14:45:15 +0300

    From content cont

--- CarvedRock.WebApp/Pages/GetInTouch.cshtml.cs
+++ CarvedRock.WebApp/Pages/GetInTouch.cshtml.cs
@@ -5,11 +5,21 @@ namespace CarvedRock.WebApp.Pages
 {
     public class GetInTouchModel : PageModel
     {
+        [BindProperty] 
+        public string Content { get; set; }
         public void OnGet()
         {
         }
-        public void OnPost()
+        public async void OnPostAsync()
         {
+
+            var bestContent = Content;
+
+            var form= await Request.ReadFormAsync();
+
+            var betterContent = form["content"];
+            var betterEmail = form["emailaddress"];
+
             var content = Request.Form["content"];
             var emaildAddress = Request.Form["emailaddress"];
         }
